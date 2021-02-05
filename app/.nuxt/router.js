9ba3684bb50ccf155e5dd6656b6cafbba66953b0
taigo-kuno <pr.huxi360@gmail.com>
2021-02-05 12:54:57 +0900

    フロントエンドの環境構築が終わりました。

---
+++ app/.nuxt/router.js
@@ -0,0 +1,68 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL, decode } from '@nuxt/ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _b51df350 = () => interopDefault(import('../pages/hello.vue' /* webpackChunkName: "pages/hello" */))
+const _42205e30 = () => interopDefault(import('../pages/todo.vue' /* webpackChunkName: "pages/todo" */))
+const _2dfb1658 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+
+// TODO: remove in Nuxt 3
+const emptyFn = () => {}
+const originalPush = Router.prototype.push
+Router.prototype.push = function push (location, onComplete = emptyFn, onAbort) {
+  return originalPush.call(this, location, onComplete, onAbort)
+}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/hello",
+    component: _b51df350,
+    name: "hello"
+  }, {
+    path: "/todo",
+    component: _42205e30,
+    name: "todo"
+  }, {
+    path: "/",
+    component: _2dfb1658,
+    name: "index"
+  }],
+
+  fallback: false
+}
+
+function decodeObj(obj) {
+  for (const key in obj) {
+    if (typeof obj[key] === 'string') {
+      obj[key] = decode(obj[key])
+    }
+  }
+}
+
+export function createRouter () {
+  const router = new Router(routerOptions)
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    const r = resolve(to, current, append)
+    if (r && r.resolved && r.resolved.query) {
+      decodeObj(r.resolved.query)
+    }
+    return r
+  }
+
+  return router
+}
---
+++ app/pages/todo.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <p>Home</p>
+    <div class="ToDoInput">
+      <p>タイトルを入力してください</p>
+      <input placeholder="title" v-model="newToDoTitle">
+      <button @click="addToDo()">追加</button>
+    </div>
+    <div class="ToDoOutput">
+      <div v-for=" (title, index) in titles" :key="title.index">
+        <p>
+          <input type="checkbox">
+          ToDo{{ index + 1 }} : {{ title }}
+        </p>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data:() => ({
+    newToDoTitle:'',
+    titles: [] as any,
+  }),
+  methods:{
+    addToDo(){
+      if(this.newToDoTitle){
+        this.titles.push(this.newToDoTitle)
+        alert(`${this.newToDoTitle}を作成しました。`)
+        this.newToDoTitle = ''
+      } else{
+        alert('タイトルを入力してください')
+      }
+    }
+  }
+})
+</script>
+
+<style >
+div.ToDoInput {
+  padding:30px;
+}
+
+div.ToDoOutput {
+  padding:40px;
+}
+</style>
